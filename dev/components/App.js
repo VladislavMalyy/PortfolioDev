04e7466de68cd4e613e9bd69f1826f16e65944e9
iMac <imac@iMac-iMac.local>
2017-09-19 16:23:19 +0300

    Add all data to site (data.js)

--- dev/components/App.js
+++ dev/components/App.js
@@ -5,6 +5,7 @@ import About from './About'
 import Skills from './Skills';
 import Projects from './Projects';
 import Footer from './Footer';
+import DATA from '../data';
 
 class App extends React.Component{
     render(){
---
+++ dev/data.js
@@ -0,0 +1,248 @@
+
+const DATA = {
+    'skills' : [
+        {
+            'id' : '0',
+            'title' : 'HTML+CSS+JS',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/html_css_js.png'
+        },
+        {
+            'id' : '1',
+            'title' : 'JavaScript 2015',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/es6.png'
+        },
+        {
+            'id' : '2',
+            'title' : 'Craft CMS',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/craft.png'
+        },
+        {
+            'id' : '3',
+            'title' : 'Gulp',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/gulp.png'
+        },
+        {
+            'id' : '4',
+            'title' : 'Webpack',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/webpack.png'
+        },
+        {
+            'id' : '5',
+            'title' : 'CSS Pre-processors',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/css_preprocessors.png'
+        },
+        {
+            'id' : 'CSS Frameworks',
+            'title' : 'HTML+CSS+JS',
+            'desc' : 'lorem insum dolor amet',
+            'imgUrl' : './images/css_frameworks.png'
+        }
+    ],
+    'projects' : [
+        {
+            'id' : '0',
+            'title' : 'Bermuda Quest',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'all',
+            'tools' : [
+                {
+                    'id' : '0',
+                    'title' : 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'SASS'
+                },
+                {
+                    'id' : '2',
+                    'title' : 'Gulp'
+                },
+                {
+                    'id' : '3',
+                    'title' : 'Twitter Bootstrap 3'
+                }
+            ]
+        },
+        {
+            'id' : '2',
+            'title' : 'AJ Recruiting',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'all',
+            'tools' : [
+                {
+                    'id' : '0',
+                    'title' : 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'SASS'
+                },
+                {
+                    'id' : '2',
+                    'title' : 'Gulp'
+                }
+            ]
+        },
+        {
+            'id' : '3',
+            'title' : 'Netoctat',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'all',
+            'tools' : [
+                {
+                    'id' : '0',
+                    'title' : 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'SASS'
+                },
+                {
+                    'id' : '2',
+                    'title' : 'Gulp'
+                }
+            ]
+        },
+        {
+            'id' : '4',
+            'title' : 'Wideweb',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'craft',
+            'tools' : [
+                {
+                    'id': '0',
+                    'title': 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'Craft'
+                }
+            ]
+        },
+        {
+            'id' : '5',
+            'title' : 'Crimea Rentime',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'craft',
+            'tools' : [
+                {
+                    'id': '0',
+                    'title': 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'Craft'
+                }
+            ]
+        },
+        {
+            'id' : '6',
+            'title' : 'Eco Vagonka',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'craft',
+            'tools' : [
+                {
+                    'id': '0',
+                    'title': 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'Craft'
+                },
+                {
+                    'id' : '3',
+                    'title' : 'Twitter Bootstrap 3'
+                }
+            ]
+        },
+        {
+            'id' : '7',
+            'title' : 'Pack',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'craft',
+            'tools' : [
+                {
+                    'id': '0',
+                    'title': 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'Craft'
+                }
+            ]
+        },
+        {
+            'id' : '8',
+            'title' : 'CSC',
+            'description' : 'Lorem inpum dolor amet',
+            'link' : '#',
+            'tasks' : 'craft',
+            'tools' : [
+                {
+                    'id': '0',
+                    'title': 'HTML+CSS+JS'
+                },
+                {
+                    'id' : '1',
+                    'title' : 'Craft'
+                }
+            ]
+        },
+        {
+            'id' : '10',
+            'title' : 'BestMebel',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '11',
+            'title' : 'Suvorovskiy',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '12',
+            'title' : 'Lisenok',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '13',
+            'title' : 'Kuzia',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '14',
+            'title' : 'Sova',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '15',
+            'title' : 'Faberlic',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '16',
+            'title' : 'AngelMu',
+            'tasks' : 'different'
+        },
+        {
+            'id' : '17',
+            'title' : 'JuliaSwed',
+            'tasks' : 'different'
+        }
+
+    ]
+};
+
+export default DATA;
